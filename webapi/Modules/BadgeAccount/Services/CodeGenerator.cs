a538f9931d6aa6e241505cb951853ac8925a3a82
Lord Turmoil <tony-turmoil@qq.com>
2023-07-04 19:35:46 +0800

    feat: add code generator

---
+++ webapi/Modules/BadgeAccount/Services/CodeGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace BadgeBoard.Api.Modules.BadgeAccount.Services
+{
+	public class CodeGenerator
+	{
+		private static readonly string CODE_CHAR_SET = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+
+		private CodeGenerator() { }
+		
+		public static string GenerateCode(int length)
+		{
+			string code = string.Empty;
+			int upper = CODE_CHAR_SET.Length;
+			
+			for (int i = 0; i < length; i++) {
+				code += CODE_CHAR_SET[(int)Random.Shared.NextInt64(upper)];
+			}
+
+			return code;
+		}
+	}
+}

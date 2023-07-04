ed8d67461a32ebaafa098343f6a2b1eaf8683fbf
Lord Turmoil <tony-turmoil@qq.com>
2023-07-04 18:34:58 +0800

    feat: add account verifier

---
+++ webapi/Modules/BadgeAccount/Services/AccountVerifier.cs
@@ -0,0 +1,26 @@
+﻿// Verify account related values
+
+using System.Text.RegularExpressions;
+
+namespace BadgeBoard.Api.Modules.BadgeAccount.Services
+{
+	public class AccountVerifier
+	{
+		private AccountVerifier() { }
+
+		public static bool VerifyPassword(string password)
+		{
+			return Regex.IsMatch(password, @"^(?=.*\d)(?=(.*\W){1})(?=.*[a-zA-Z])(?!.*\s).{1,16}$", RegexOptions.IgnoreCase);
+		}
+
+		public static bool VerifyEmail(string email)
+		{
+			return Regex.IsMatch(email, @"^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$", RegexOptions.IgnoreCase);
+		}
+
+		public static bool VerifyAccount(string password, string email)
+		{
+			return VerifyPassword(password) && VerifyEmail(email);
+		}
+	}
+}

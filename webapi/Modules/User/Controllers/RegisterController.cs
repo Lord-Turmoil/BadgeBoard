7dae2f4229769ab37a289e85225a3836a2f9a4ac
Lord Turmoil <tony-turmoil@qq.com>
2023-07-04 22:13:19 +0800

    feat: dependency injection ok

--- webapi/Modules/User/Controllers/RegisterController.cs
+++ webapi/Modules/User/Controllers/RegisterController.cs
@@ -1,29 +1,41 @@
-﻿using BadgeBoard.Api.Extensions.Response;
+﻿using BadgeBoard.Api.Extensions.Email;
+using BadgeBoard.Api.Extensions.Response;
 using BadgeBoard.Api.Modules.BadgeAccount.Models;
+using BadgeBoard.Api.Modules.BadgeGlobal;
+using BadgeBoard.Api.Modules.User.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BadgeBoard.Api.Modules.User.Controllers
 {
 	[Route("api/[controller]")]
-    [ApiController]
-    public class RegisterController : Controller
-    {
-        [HttpPost]
-        [Route("code")]
-        public async Task<BadgeResponse> SendVerificationCode([FromQuery] string? type)
-        {
-			int emailType = EmailTypes.Invalid;
-            if ("register".Equals(type)) {
-				emailType = EmailTypes.Register;
-            } else if ("retrieve".Equals(type)) {
-				emailType = EmailTypes.Retrieve;
-            }
+	[ApiController]
+	public class RegisterController : Controller
+	{
+		private readonly IServiceProvider _provider;
 
-            if (emailType == EmailTypes.Invalid) {
-                return new BadRequestResponse(new BadRequestDto($"Invalid type: {type}"));
-            }
-
-            return new GoodResponse(new GoodDto());
+		public RegisterController(IServiceProvider provider)
+		{
+			_provider = provider;
 		}
-    }
+
+		[HttpPost]
+		[Route("code")]
+		public async Task<BadgeResponse> SendVerificationCode([FromQuery] string? type)
+		{
+			int emailType = EmailTypes.Invalid;
+			if ("register".Equals(type)) {
+				emailType = EmailTypes.Register;
+			} else if ("retrieve".Equals(type)) {
+				emailType = EmailTypes.Retrieve;
+			}
+
+			if (emailType == EmailTypes.Invalid) {
+				return new BadRequestResponse(new BadRequestDto($"Invalid type: {type}"));
+			}
+
+			new UserEmailService(_provider).SendRegistrationEmailAsync("123@123", "123");
+
+			return new GoodResponse(new GoodDto());
+		}
+	}
 }

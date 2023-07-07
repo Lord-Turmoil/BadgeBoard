ddca94c583f12d3af960f89af730fb806b5d6c6b
Lord Turmoil <tony-turmoil@qq.com>
2023-07-07 19:17:59 +0800

    feat: add JWT util

---
+++ webapi/Extensions/Jwt/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BadgeBoard.Api.Extensions.Jwt
+{
+	public class JwtOptions
+	{
+		public const string JwtSection = "JwtOptions";
+
+		public string Token { get; set; }
+	}
+}
---
+++ webapi/Extensions/Jwt/JwtUtil.cs
@@ -0,0 +1,26 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BadgeBoard.Api.Extensions.Jwt
+{
+	public static class JwtUtil
+	{
+		public static string CreateToken(IOptions<JwtOptions> options, string username)
+		{
+			List<Claim> claims = new List<Claim> {
+				new Claim(ClaimTypes.Name, username)
+			};
+
+			var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(options.Value.Token));
+			var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+			var token = new JwtSecurityToken(
+				claims: claims,
+				expires: DateTime.Now.AddDays(7),
+				signingCredentials: credential);
+
+			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+	}
+}

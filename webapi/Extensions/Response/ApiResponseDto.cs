0f45ce95e956ca41780c80a76caf7bae80e3b200
Lord Turmoil <tony-turmoil@qq.com>
2023-07-07 19:17:52 +0800

    refactor: response dto

--- webapi/Extensions/Response/ApiResponseDto.cs
+++ webapi/Extensions/Response/ApiResponseDto.cs
@@ -2,5 +2,31 @@
 {
 	public class ApiResponseDto
 	{
+		public ApiResponseMeta Meta { get; set; }
+		public object? Data { get; set; }
+
+		protected ApiResponseDto(int status, string? message = null, object? data = null)
+		{
+			Meta = new ApiResponseMeta(status, message);
+			Data = data;
+		}
+	}
+
+	public class ApiResponseMeta
+	{
+		public int Status { get; set; }
+		public string? Message { get; set; }
+
+		public ApiResponseMeta()
+		{
+			Status = 0;
+			Message = null;
+		}
+
+		public ApiResponseMeta(int status, string? message = null)
+		{
+			Status = status;
+			Message = message;
+		}
 	}
 }
--- webapi/Extensions/Response/DefaultResponseDto.cs
+++ webapi/Extensions/Response/DefaultResponseDto.cs
@@ -1,35 +1,35 @@
 ﻿namespace BadgeBoard.Api.Extensions.Response
 {
 	// Dto that does have data
-	public class OrdinaryDto : ApiResponseData
+	public class OrdinaryDto : ApiResponseDto
 	{
 		public OrdinaryDto(int status, string? message = null) : base(status, message, null)
 		{
 		}
 	}
 
-	public class BadDto : ApiResponseData
+	public class BadDto : ApiResponseDto
 	{
 		public BadDto(int status, string message = "Bad request", object? data = null) : base(status, message, data)
 		{
 		}
 	}
 
-	public class GoodDto : ApiResponseData
+	public class GoodDto : ApiResponseDto
 	{
 		public GoodDto(string message = "Nice request", object? data = null) : base(0, message, data)
 		{
 		}
 	}
 
-	public class BadRequestDto : ApiResponseData
+	public class BadRequestDto : ApiResponseDto
 	{
 		public BadRequestDto(string message = "Request format error", object? data = null) : base(-1, message, data)
 		{
 		}
 	}
 
-	public class InternalServerErrorDto : ApiResponseData
+	public class InternalServerErrorDto : ApiResponseDto
 	{
 		public InternalServerErrorDto(string message = "Unexpected error", object? data = null) : base(-2, message, data)
 		{

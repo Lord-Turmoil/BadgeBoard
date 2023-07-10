9912c0a83252375e1c5637feaf4bd8731cf39c3c
Lord Turmoil <tony-turmoil@qq.com>
2023-07-10 18:05:54 +0800

    update links

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -5,21 +5,22 @@ import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
 import UserPage from './pages/UserPage'
 import NotFoundPage from './pages/NotFoundPage'
-import NavBarDev from './parts/NavBarDev'
+import SignUpPage from './pages/SignUpPage'
+import { Component } from 'react'
 
-function App() {
-    return (
-        <div>
-            <NavBarDev></NavBarDev>
-            <Routes>
-                <Route exact path="/" element={<HomePage></HomePage>}></Route>
-                <Route exact path="/register" element={<RegisterPage></RegisterPage>}></Route>
-                <Route exact path="/login" element={<LoginPage></LoginPage>}></Route>
-                <Route exact path="/user/:uid" element={<UserPage></UserPage>}></Route>
-                <Route exact path="*" element={<NotFoundPage></NotFoundPage>}></Route>
-            </Routes>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Routes>
+                    <Route exact path="/" element={<HomePage></HomePage>}></Route>
+                    <Route exact path="/register" element={<RegisterPage></RegisterPage>}></Route>
+                    <Route exact path="/signup" element={<SignUpPage></SignUpPage>}></Route>
+                    <Route exact path="/login" element={<LoginPage></LoginPage>}></Route>
+                    <Route exact path="/user/:uid" element={<UserPage></UserPage>}></Route>
+                    <Route exact path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+                </Routes>
+            </div>
+        );
+    }
 }
-
-export default App;
--- reactapp/src/parts/NavBarDev.jsx
+++ reactapp/src/parts/NavBarDev.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 
 function NavBarDev() {
     return (
-        <div>
+        <div id="nav-dev">
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/register">Register</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
                 <li><Link to="/login">Login</Link></li>
                 <li><Link to="/user/0">User 0</Link></li>
                 <li><Link to="/404">404</Link></li>

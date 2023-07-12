a7c3854cd7262a11415e707dbb4a27d719a33571
Lord Turmoil <tony-turmoil@qq.com>
2023-07-12 10:42:22 +0800

    fix: verification bug

--- webapi/Modules/BadgeUser/Services/Utils/AccountVerifier.cs
+++ webapi/Modules/BadgeUser/Services/Utils/AccountVerifier.cs
@@ -9,7 +9,7 @@ namespace BadgeBoard.Api.Modules.BadgeUser.Services.Utils
         public static bool VerifyPassword(string? password)
         {
 	        //return !string.IsNullOrEmpty(password) && Regex.IsMatch(password, @"^(?=.*\d)(?=(.*\W){1})(?=.*[a-zA-Z])(?!.*\s).{6,16}$", RegexOptions.IgnoreCase);
-	        return !string.IsNullOrEmpty(password) && Regex.IsMatch(password, @"[a-z0-9_-]{6,16}$", RegexOptions.IgnoreCase);
+	        return !string.IsNullOrEmpty(password) && Regex.IsMatch(password, @"[a-zA-Z0-9_-]{6,16}$", RegexOptions.IgnoreCase);
         }
 
         public static bool VerifyEmail(string? email)
@@ -19,7 +19,7 @@ namespace BadgeBoard.Api.Modules.BadgeUser.Services.Utils
 
         public static bool VerifyUsername(string? username)
         {
-            return !string.IsNullOrEmpty(username) && Regex.IsMatch(username, @"^[ a-z0-9_-]{3,20}$", RegexOptions.IgnoreCase);
+            return !string.IsNullOrEmpty(username) && Regex.IsMatch(username, @"^[ a-zA-Z0-9_-]{3,20}$", RegexOptions.IgnoreCase);
         }
 
         public static bool VerifyAccount(string? username, string? password, string? email = null)

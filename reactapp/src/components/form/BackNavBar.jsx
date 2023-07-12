a46d17d9f828497625cdb8ee04de67e7eb2cd2ac
Lord Turmoil <tony-turmoil@qq.com>
2023-07-12 18:02:03 +0800

    update: nav bar

--- reactapp/src/components/form/BackNavBar.jsx
+++ reactapp/src/components/form/BackNavBar.jsx
@@ -1,15 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import { Fab } from "@mui/material";
+import { Fab } from '@mui/material';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
 
 export default function BackNavBar() {
     var navigate = useNavigate();
 
     return (
-        <div style={{width: '90%', margin: '10px auto'}}>
-            <Fab size='medium' color='primary' onClick={() => { navigate(-1); }}>
+        <div style={{ width: '90%', margin: '10px auto' }}>
+            <Fab size="medium" color="primary" onClick={() => { navigate(-1); }}>
                 <ArrowBackIosRoundedIcon />
+            </Fab>
+            <Fab size="medium" sx={{ marginLeft: '10px' }} color="primary" onClick={() => { navigate("/"); }}>
+                <HomeRoundedIcon />
             </Fab>
         </div>
     );

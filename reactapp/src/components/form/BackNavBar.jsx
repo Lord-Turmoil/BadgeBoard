77016908e349df7dc0ed5a7efc3dd472f68a7685
Lord Turmoil <tony-turmoil@qq.com>
2023-07-12 11:55:41 +0800

    update: navigate back

--- reactapp/src/components/form/BackNavBar.jsx
+++ reactapp/src/components/form/BackNavBar.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import { Fab } from "@mui/material";
 import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
 
 export default function BackNavBar() {
+    var navigate = useNavigate();
+
     return (
         <div style={{width: '90%', margin: '10px auto'}}>
-            <Fab size='medium' color='primary'>
+            <Fab size='medium' color='primary' onClick={() => { navigate(-1); }}>
                 <ArrowBackIosRoundedIcon />
             </Fab>
         </div>

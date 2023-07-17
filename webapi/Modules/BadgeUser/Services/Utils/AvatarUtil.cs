d256d856e5ea0a2bddfb769ec37123c8509fdec3
Lord Turmoil <tony-turmoil@qq.com>
2023-07-17 12:29:57 +0800

    feat: add AvatarUtil

---
+++ webapi/Modules/BadgeUser/Services/Utils/AvatarUtil.cs
@@ -0,0 +1,28 @@
+﻿namespace BadgeBoard.Api.Modules.BadgeUser.Services.Utils
+{
+	public static class AvatarUtil
+	{
+		public const string AvatarRootPath = "wwwroot/";
+
+		public static bool DeleteAvatar(string? avatarUrl)
+		{
+			if (avatarUrl == null) {
+				return true;
+			}
+
+			var fullPath = Path.Join(AvatarRootPath, avatarUrl);
+			if (!File.Exists(fullPath)) {
+				return true;
+			}
+
+			try {
+				File.Delete(fullPath);
+			} catch (Exception ex) {
+				Console.WriteLine(ex.ToString());
+				return false;
+			}
+
+			return true;
+		}
+	}
+}

a434c7e219fcdf1373743abab1757a8fbb6047a5
Lord Turmoil <tony-turmoil@qq.com>
2023-07-19 10:47:50 +0800

    feat: add key generator

---
+++ webapi/Modules/BadgeGlobal/Utils/KeyGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace BadgeBoard.Api.Modules.BadgeGlobal.Utils
+{
+	public static class KeyGenerator
+	{
+		private const int MinUserId = 1000000000;
+		private const int MaxUserId = 2000000000;
+
+		public static int GenerateKey()
+		{
+			return (int)Random.Shared.NextInt64(MinUserId, MaxUserId + 1);
+		}
+	}
+}

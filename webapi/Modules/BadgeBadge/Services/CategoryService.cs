a268bd32e94fb7c0ff9d9bb8bbd7345726c5a35c
Lord Turmoil <tony-turmoil@qq.com>
2023-07-20 17:41:25 +0800

    feat: category exists

---
+++ webapi/Modules/BadgeBadge/Services/CategoryService.cs
@@ -0,0 +1,55 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using AutoMapper;
+using BadgeBoard.Api.Extensions.Module;
+using BadgeBoard.Api.Extensions.Response;
+using BadgeBoard.Api.Modules.BadgeBadge.Models;
+using BadgeBoard.Api.Modules.BadgeBadge.Services.Utils;
+using BadgeBoard.Api.Modules.BadgeUser.Dtos;
+using BadgeBoard.Api.Modules.BadgeUser.Models;
+
+namespace BadgeBoard.Api.Modules.BadgeBadge.Services
+{
+	public class CategoryService : BaseService, ICategoryService
+	{
+		protected CategoryService(IServiceProvider provider, IUnitOfWork unitOfWork, IMapper mapper) : base(provider, unitOfWork, mapper)
+		{
+		}
+
+		public async Task<ApiResponse> Exists(int id, string name)
+		{
+			var repo = _unitOfWork.GetRepository<User>();
+			var user = await User.FindAsync(repo, id);
+			if (user == null) {
+				return new GoodResponse(new UserNotExistsDto());
+			}
+
+			var categories = await CategoryUtil.GetCategoryOfUserAsync(_unitOfWork.GetRepository<Category>(), user.Id);
+			return new GoodResponse(new GoodWithDataDto(categories.Any(x => x.Name == name)));
+		}
+
+		public Task<ApiResponse> AddCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ApiResponse> DeleteCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ApiResponse> RenameCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ApiResponse> UpdateCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ApiResponse> MergeCategory(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ webapi/Modules/BadgeBadge/Services/Utils/CategoryUtil.cs
@@ -0,0 +1,13 @@
+﻿using Arch.EntityFrameworkCore.UnitOfWork;
+using BadgeBoard.Api.Modules.BadgeBadge.Models;
+
+namespace BadgeBoard.Api.Modules.BadgeBadge.Services.Utils
+{
+	public static class CategoryUtil
+	{
+		public static async Task<IList<Category>> GetCategoryOfUserAsync(IRepository<Category> repo, int id)
+		{
+			return await repo.GetAllAsync(predicate: x => x.UserId == id);
+		}
+	}
+}

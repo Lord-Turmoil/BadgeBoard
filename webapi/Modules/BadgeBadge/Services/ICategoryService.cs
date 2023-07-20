0ed38a7959d7486d2abdf911f45f0445b5f7bfac
Lord Turmoil <tony-turmoil@qq.com>
2023-07-20 17:20:56 +0800

    feat: category service interface

--- webapi/Modules/BadgeBadge/Services/ICategoryService.cs
+++ webapi/Modules/BadgeBadge/Services/ICategoryService.cs
@@ -3,10 +3,28 @@
 namespace BadgeBoard.Api.Modules.BadgeBadge.Services
 {
 	/// <summary>
-	/// Creation and deletion of category.
+	/// Creation and deletion of category. id is from the request
+	/// authorization, denoting the user who is making change of his/her
+	/// own categories.
 	/// </summary>
 	public interface ICategoryService
 	{
+		public Task<ApiResponse> Exists(int id, string name);
+
 		public Task<ApiResponse> AddCategory(int id);
+		
+		// Delete a category, can optionally merge data into another.
+		public Task<ApiResponse> DeleteCategory(int id);
+		
+		// Only rename the category.
+		public Task<ApiResponse> RenameCategory(int id);
+
+		// Update category options. Won't affect category name and
+		// current badges in it.
+		public Task<ApiResponse> UpdateCategory(int id);
+
+		// Merge two categories, and abandon (delete) the merge source.
+		// For now, will ignore target category options. :(
+		public Task<ApiResponse> MergeCategory(int id);
 	}
 }

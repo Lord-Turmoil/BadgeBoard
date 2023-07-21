7ed1c23f892ace1b71880104e8458d2195be96d5
Lord Turmoil <tony-turmoil@qq.com>
2023-07-21 15:44:39 +0800

    feat: badge service interface

--- webapi/Modules/BadgeBadge/Services/IBadgeService.cs
+++ webapi/Modules/BadgeBadge/Services/IBadgeService.cs
@@ -5,16 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace BadgeBoard.Api.Modules.BadgeBadge.Services
 {
 	/// <summary>
-	/// For the creation of badges.
+	/// For the creation of badges. All id is the sender's id. For requests
+	/// that can be anonymous (only add badge), 0 means anonymous.
 	/// </summary>
 	public interface IBadgeService
 	{
-		// Here, all id is the sender's id. For requests that can
-		// be anonymous (only add badge) 0 means anonymous.
-
 		public Task<ApiResponse> AddQuestionBadge(int id, AddQuestionBadgeDto dto);
 		public Task<ApiResponse> AddMemoryBadge(int id, AddMemoryBadgeDto dto);
 
 		public Task<ApiResponse> DeleteBadge(int id, DeleteBadgeDto dto);
+
+		public Task<ApiResponse> UpdateQuestionBadge(int id);
+		public Task<ApiResponse> UpdateMemoryBadge(int id);
+
+		// Move badge to another category.
+		public Task<ApiResponse> MoveBadge(int id);
 	}
 }

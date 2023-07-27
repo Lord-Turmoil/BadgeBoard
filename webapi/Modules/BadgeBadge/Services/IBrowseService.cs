290d33469c71042d77148b45948939f74dc208e5
Lord Turmoil <tony-turmoil@qq.com>
2023-07-27 20:26:15 +0800

    feat: add browse service interface

--- webapi/Modules/BadgeBadge/Services/IBrowseService.cs
+++ webapi/Modules/BadgeBadge/Services/IBrowseService.cs
@@ -2,9 +2,25 @@
 // Licensed under the BSD 2-Clause License.
 
 using BadgeBoard.Api.Extensions.Module;
+using BadgeBoard.Api.Extensions.Response;
 
 namespace BadgeBoard.Api.Modules.BadgeBadge.Services;
 
+/// <summary>
+///     Functions without id parameter get badges of other users. While
+///     those with id get badges of user him/her self, where id is the
+///     initiator's user id, or, a user with administrative permission.
+/// </summary>
 public interface IBrowseService : IService
 {
+    Task<ApiResponse> GetBadge(int badgeId);
+    Task<ApiResponse> GetBadge(int id, int badgeId);
+
+    Task<ApiResponse> GetBadgesOfUser(int userId);
+    Task<ApiResponse> GetBadgesOfUser(int id, int userId);
+
+
+    // categoryId == 0 means default category
+    Task<ApiResponse> GetBadgesOfCategory(int userId, int categoryId);
+    Task<ApiResponse> GetBadgesOfCategory(int id, int userId, int categoryId);
 }

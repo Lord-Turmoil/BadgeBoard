3d230edcb422595fa812c9d19d1d1fb003480ca3
Lord Turmoil <tony-turmoil@qq.com>
2023-07-28 10:30:17 +0800

    refactor: reformat some file

---
+++ webapi/Modules/BadgeGlobal/Dtos/FailedToSaveChangesDto.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (C) 2018 - 2023 Tony's Studio. All rights reserved.
+// Licensed under the BSD 2-Clause License.
+
+using BadgeBoard.Api.Extensions.Response;
+
+namespace BadgeBoard.Api.Modules.BadgeGlobal.Dtos;
+
+public class FailedToSaveChangesDto : ApiResponseDto
+{
+    public FailedToSaveChangesDto(string? message = "Failed to save changes", object? data = null) : base(
+        Errors.FailedToSaveChanges, message, data) { }
+}
--- webapi/Modules/BadgeGlobal/Dtos/MissingReferenceDto.cs
+++ webapi/Modules/BadgeGlobal/Dtos/MissingReferenceDto.cs
@@ -5,12 +5,6 @@ using BadgeBoard.Api.Extensions.Response;
 
 namespace BadgeBoard.Api.Modules.BadgeGlobal.Dtos;
 
-public class FailedToSaveChangesDto : ApiResponseDto
-{
-    public FailedToSaveChangesDto(string? message = "Failed to save changes", object? data = null) : base(
-        Errors.FailedToSaveChanges, message, data) { }
-}
-
 public class MissingReferenceDto : ApiResponseDto
 {
     public MissingReferenceDto(string? message = "Missing reference", object? data = null) : base(

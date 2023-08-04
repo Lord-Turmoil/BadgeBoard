1852200adffd82fe1251586fbb77b971b2ca606a
Lord Turmoil <tony-turmoil@qq.com>
2023-08-04 12:41:26 +0800

    feat: add avatar to badge

--- reactapp/src/components/display/Note/QuestionNote/QuestionNote.jsx
+++ reactapp/src/components/display/Note/QuestionNote/QuestionNote.jsx
@@ -1,17 +1,24 @@
 import './QuestionNote.css';
 import '../NoteDecoration.css';
+import { Avatar } from '@mui/material';
+import AvatarUtil from '~/services/user/AvatarUtil';
 
+// sender & receiver should be (full user)
 export default function QuestionNote({
     question = "",
-    answer = null
+    answer = null,
+    sender = null,
+    receiver = null
 }) {
     return (
         <div className="Note QuestionNote">
             <div className="QuestionNote__question Note__boxWithSeparator">
+                {sender && <Avatar src={AvatarUtil.getUrlFromUser(sender)} />}
                 <h3>{question}</h3>
             </div>
             <div className="QuestionNote__answer">
                 <p className={answer ? null : 'empty'}>{answer ?? "Not answered yet..."}</p>
+                <Avatar className='QuestionNote__avatar' src={AvatarUtil.getUrlFromUser(receiver)} />
             </div>
         </div>
     );
